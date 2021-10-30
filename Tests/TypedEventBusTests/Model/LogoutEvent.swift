cb7828d072ba37a6eb08ea1a042610886dae348a
Balazs Kanyo <bkanyo@gmail.com>
2021-10-30 09:36:51 +0200

    test cases extended

---
+++ Tests/TypedEventBusTests/Model/LogoutEvent.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  
+//
+//  Created by Balazs Kanyo on 2021. 10. 30..
+//
+
+import Foundation
+import TypedEventBus
+
+final class LogoutEvent: TypedEvent<Person> {}
--- Tests/TypedEventBusTests/TypedEventBusTests.swift
+++ Tests/TypedEventBusTests/TypedEventBusTests.swift
@@ -6,38 +6,52 @@ final class TypedEventBusTests: XCTestCase {
     var cancellables = Set<AnyCancellable>()
 
     func testWithEvent() throws {
-        let callExpectation = XCTestExpectation(description: "subscribe called")
+        let callExpectationLogin = XCTestExpectation(description: "subscribe called - login")
+        let callExpectationLogout = XCTestExpectation(description: "subscribe called - logout")
 
         let loginEvent = LoginEvent()
         let stateChangedEvent = ApplicationStateChangedEvent()
+        let logoutEvent = LogoutEvent()
         loginEvent.subscribeEvent { event in
             XCTAssertEqual(event.object?.name ?? "", "Joe")
-            callExpectation.fulfill()
+            callExpectationLogin.fulfill()
+        }.store(in: &cancellables)
+
+        logoutEvent.subscribeEvent { event in
+            XCTAssertEqual(event.object?.name ?? "", "Martin")
+            callExpectationLogout.fulfill()
         }.store(in: &cancellables)
 
 
         stateChangedEvent.post(.authenticated)
+        logoutEvent.post(Person(name: "Martin"))
         DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(100)) {
-            //        loginEvent.post(LoginEvent(Person(name: "Joe")))
             loginEvent.post(Person(name: "Joe"))
         }
 
-        wait(for: [callExpectation], timeout: 2)
+        wait(for: [callExpectationLogout, callExpectationLogin], timeout: 2)
     }
 
     func testWithEventBus() {
-        let callExpectation = XCTestExpectation(description: "subscribe called")
+        let callExpectationLogin = XCTestExpectation(description: "subscribe called - login")
+        let callExpectationLogout = XCTestExpectation(description: "subscribe called - logout")
 
         let eventBus = TypedEventBus()
         eventBus.subscribe(to: LoginEvent.self) { object in
             XCTAssertEqual(object.name, "Joe")
-            callExpectation.fulfill()
+            callExpectationLogin.fulfill()
+        }.store(in: &cancellables)
+
+        eventBus.subscribe(to: LogoutEvent.self) { person in
+            XCTAssertEqual(person.name, "Mary")
+            callExpectationLogout.fulfill()
         }.store(in: &cancellables)
 
         eventBus.post(ApplicationStateChangedEvent(.authenticated))
+        eventBus.post(LogoutEvent(Person(name: "Mary")))
         DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(100)) {
             eventBus.post(LoginEvent(Person(name: "Joe")))
         }
-        wait(for: [callExpectation], timeout: 2)
+        wait(for: [callExpectationLogout, callExpectationLogin], timeout: 2)
     }
 }
